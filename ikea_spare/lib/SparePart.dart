39003fcb8414a7cea5ad6132ed170ef6b22bb0a9
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-09-26 16:18:43 +0200

    Added SparePart class and Parts collection

--- ikea_spare/lib/SparePart.dart
+++ ikea_spare/lib/SparePart.dart
@@ -1,23 +1,10 @@
 class SparePart {
-  String name;
-  int id;
-  String description;
-  String imageUrl;
-  double price;
-  int quantity;
+  String id = "productID";
+  String name = "defaultName";
+  String description = "defaultDescription";
+  String imageUrl = "https://i.kym-cdn.com/entries/icons/facebook/000/020/260/nilesyy-nilez.jpg";
+  double price = 0;
+  int quantity = 0;
 
-  SparePart({this.name = "defaultName", this.id = 666, this.description = "defaultDesc", this.imageUrl = "https://ikea.sparepartservice.shop/pics/eshop/120.gif", this.price = 0, this.quantity = 0});
-
-  void incrementQuantity() {
-    quantity++;
-  }
-
-  void addQuantity(int quantityToAdd) {
-    quantity += quantityToAdd;
-  }
-
-  void removeQuantity(int quantityToRemove) {
-    quantity -= quantityToRemove;
-  }
-
+  SparePart(this.id, this.name, this.description, this.imageUrl, this.price, this.quantity);
 }

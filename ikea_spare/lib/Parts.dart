6b0ad40b8dee4238c265916098e8178656408a75
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-09-27 11:16:03 +0200

    image

--- ikea_spare/lib/Parts.dart
+++ ikea_spare/lib/Parts.dart
@@ -7,10 +7,10 @@ class Parts {
   static List<SparePart> getSpareParts() {
 
       SparePart part1 = SparePart("761310", "Foot", "Denna artikel är kompatibel / passar för modellen: 00015068 / IKEA",
-       "https://media.istockphoto.com/id/183289521/sv/foto/number-1.jpg?s=612x612&w=0&k=20&c=o1qe3GLjyIVsQGUTvrIKls2cHD9MQLDrnABefWrd44E=", 1);
+       "https://cdn.fixpart.net/126375/f1107baf9c6811d82078c4131e5a3d5c.jpg", 1);
 
       SparePart part2 = SparePart("H191825", "Cap White", "Ersätter: #759530 C00336008 481946248175 STOPPER. Denna artikel är kompatibel / passar för modellen: 0015068 / IKEA",
-       "https://image.spreadshirtmedia.net/image-server/v1/products/T1459A839PA4459PT28D195580702W8333H10000/views/1,width=550,height=550,appearanceId=839,backgroundColor=F2F2F2/nummer-2-klistermaerke.jpg", 10);
+       "https://us-tuna-sounds-images.voicemod.net/3ba22705-8a5a-4ff1-9f70-c291c63fabae-1659296288122.png", 10);
 
       List<SparePart> partsList = List.empty(growable: true);
       partsList.add(part1); 

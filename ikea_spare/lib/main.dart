c864bd86eb5530d701e572ee1b7ee0f0940bc08d
pontus <pontusgi@chalmer.se>
2024-09-27 14:37:52 +0200

    TEST

--- ikea_spare/lib/main.dart
+++ ikea_spare/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ikea_spare/Widgets/ScannedPart.dart';
 import 'package:ikea_spare/Widgets/SparePartList.dart';
 import 'Widgets/ListCard.dart'; // Import your ItemCard
 
@@ -121,11 +122,13 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }

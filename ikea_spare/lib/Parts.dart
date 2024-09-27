89cfb2a0fea42b8ef069f707805d8e8fece21793
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-09-27 11:06:13 +0200

    Remove price

--- ikea_spare/lib/Parts.dart
+++ ikea_spare/lib/Parts.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:ikea_spare/SparePart.dart';
 
 class Parts {
@@ -5,14 +7,18 @@ class Parts {
   static List<SparePart> getSpareParts() {
 
       SparePart part1 = SparePart("761310", "Foot", "Denna artikel är kompatibel / passar för modellen: 00015068 / IKEA",
-       "https://media.istockphoto.com/id/183289521/sv/foto/number-1.jpg?s=612x612&w=0&k=20&c=o1qe3GLjyIVsQGUTvrIKls2cHD9MQLDrnABefWrd44E=", 72.14, 1);
+       "https://media.istockphoto.com/id/183289521/sv/foto/number-1.jpg?s=612x612&w=0&k=20&c=o1qe3GLjyIVsQGUTvrIKls2cHD9MQLDrnABefWrd44E=", 1);
 
       SparePart part2 = SparePart("H191825", "Cap White", "Ersätter: #759530 C00336008 481946248175 STOPPER. Denna artikel är kompatibel / passar för modellen: 0015068 / IKEA",
-       "https://image.spreadshirtmedia.net/image-server/v1/products/T1459A839PA4459PT28D195580702W8333H10000/views/1,width=550,height=550,appearanceId=839,backgroundColor=F2F2F2/nummer-2-klistermaerke.jpg", 77.36, 10);
+       "https://image.spreadshirtmedia.net/image-server/v1/products/T1459A839PA4459PT28D195580702W8333H10000/views/1,width=550,height=550,appearanceId=839,backgroundColor=F2F2F2/nummer-2-klistermaerke.jpg", 10);
 
       List<SparePart> partsList = List.empty(growable: true);
       partsList.add(part1); 
       partsList.add(part2);
+
+      //Map<String, SparePart> partsMap = HashMap(); 
+      //partsMap.addAll({part1.id: part1});
+      //partsMap.addAll({part2.id: part2});
 
   return partsList;
 }
--- ikea_spare/lib/SparePart.dart
+++ ikea_spare/lib/SparePart.dart
@@ -2,9 +2,8 @@ class SparePart {
   String id = "productID";
   String name = "defaultName";
   String description = "defaultDescription";
-  String imageUrl = "https://i.kym-cdn.com/entries/icons/facebook/000/020/260/nilesyy-nilez.jpg";
-  double price = 0;
+  String imageUrl = "https://www.ikea.com/global/assets/logos/brand/ikea.svg";
   int quantity = 0;
 
-  SparePart(this.id, this.name, this.description, this.imageUrl, this.price, this.quantity);
+  SparePart(this.id, this.name, this.description, this.imageUrl, this.quantity);
 }

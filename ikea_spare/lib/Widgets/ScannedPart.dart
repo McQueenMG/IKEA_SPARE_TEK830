5b59044a4d8c9df363f6151ad02f2ed0a99acb7d
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-09-30 15:53:03 +0200

    Created the scannedPart with 2 buttons and some containers

--- ikea_spare/lib/Widgets/ScannedPart.dart
+++ ikea_spare/lib/Widgets/ScannedPart.dart
@@ -1,60 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ScannedPart extends StatefulWidget {
-  const ScannedPart(
-      {Key? key,
-      required this.title,
-      required this.image})
-      : super(key: key);
+class ScannedPart extends StatelessWidget {
+  const ScannedPart({super.key, required this.name, required this.image, required this.quantity});
 
-  final String title;
+  final String name;
   final String image;
-  @override
-  State<ScannedPart> createState() => ScannedPartState();
- 
-}
-class ScannedPartState extends State<ScannedPart> {
+  final int quantity;
+
+  //@override
+  //State<ScannedPart> createState() => ScannedPartState();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      height: 250,
-      child: Card(
-        color: Colors.lightBlue[50],
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 150, // Fixed width for the image
-              height: double.infinity, // Fixed height for the image
-              child: Image.network(
-                widget.image,
-                fit: BoxFit.cover, // Adjust to cover the container
-              ),
-            ),
-            Expanded(
-              // Use Expanded to fill remaining space
-              child: Padding(
-                padding: const EdgeInsets.all(8.0), // Add padding for text
-                child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Align text to the start
-                  children: [
-                    Text(
-                      widget.title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: false,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 4), // Space between title and subtitle
-                    
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+      height: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("HEHEJEH"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: go,
+                child: const Text("Add")
+              ), 
+              ElevatedButton(
+                onPressed: go,
+                child: const Text("Remove")
+              ), 
+              ],
+          ),
+        ],
       ),
     );
-  }}
+  }
+
+  void go() {
+    
+  }
+}

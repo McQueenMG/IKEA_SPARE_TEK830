c034bd47057d05f416af0693fcc2dabc1f45cb6c
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-10-07 16:42:05 +0200

    Repo fix initial commit

---
+++ ikea_spare/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ikea_spare/Widgets/Pages/HomePage.dart'; // Import the new file where the homepage logic is.
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'IKEA SPARE (Spare Parts acquisition and resource exchange)'),
+    );
+  }
+}

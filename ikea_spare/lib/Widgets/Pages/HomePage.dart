190c243c45c4c5b82382193f98462841a8fafcbb
pontus <pontusgi@chalmer.se>
2024-10-08 19:02:56 +0200

    Update HomePage.dart

--- ikea_spare/lib/Widgets/Pages/HomePage.dart
+++ ikea_spare/lib/Widgets/Pages/HomePage.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           children: [
             SizedBox(
               width: 600,

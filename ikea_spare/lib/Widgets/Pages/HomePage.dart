4878c9b18221dd222b29554017040e4bcdc86dd7
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-10-10 11:52:57 +0200

    Changed header width

--- ikea_spare/lib/Widgets/Pages/HomePage.dart
+++ ikea_spare/lib/Widgets/Pages/HomePage.dart
@@ -66,10 +66,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     padding: const EdgeInsets.only(left: 16.0),
                     child: ConstrainedBox(
                       constraints: BoxConstraints(
-                        maxWidth: width * 0.75,
+                        maxWidth: width * 0.5,
                         maxHeight: height * 0.035,
                       ),
-                      child: SparePartListHeader(),
+                      child: const SparePartListHeader(),
                     ),
                   ),
                 ],

49e89bb8504615f20ec18fc1ab97f3afc8347320
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-10-10 11:36:30 +0200

    Added list header

--- ikea_spare/lib/Widgets/Pages/HomePage.dart
+++ ikea_spare/lib/Widgets/Pages/HomePage.dart
@@ -42,6 +42,25 @@ class _MyHomePageState extends State<MyHomePage> {
               child: CustomSearchBar(onSearch: _onSearchChanged),
             ),
             Flexible(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: width * 0.75,
+                        maxHeight: height * 0.035,
+                      ),
+                      child: SparePartList().getListHeader(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 10,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
--- ikea_spare/lib/Widgets/SparePartList.dart
+++ ikea_spare/lib/Widgets/SparePartList.dart
@@ -24,11 +24,68 @@ class SparePartList {
       itemBuilder: (_, index) {
         return Column(
           children: [
-            if (index < products.length)
-              DropdownCard(product: products[index]),
+            if (index < products.length) DropdownCard(product: products[index]),
           ],
         );
       },
+    );
+  }
+
+  Widget getListHeader() {
+    return Row(
+      children: [
+        Container(
+          width: 78, // Fixed width for the image
+        ),
+        const VerticalDivider(),
+        const Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              "Name",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              softWrap: false,
+            ),
+          ),
+        ),
+        const VerticalDivider(),
+        const Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              "ID",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              softWrap: false,
+            ),
+          ),
+        ),
+        const VerticalDivider(),
+        const Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              "Location",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              softWrap: false,
+            ),
+          ),
+        ),
+        const VerticalDivider(),
+        const Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              "Quantity",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              softWrap: false,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

7e9672726b01789aceb7634d8eccb5677b3c34a0
pontus <pontusgi@chalmer.se>
2024-10-14 11:03:27 +0200

    fiddled with buttons

--- ikea_spare/lib/Widgets/ScannedPart.dart
+++ ikea_spare/lib/Widgets/ScannedPart.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ScannedPart extends StatelessWidget {
+
   const ScannedPart({super.key, required this.name, required this.image, required this.quantity});
 
   final String name;
   final String image;
   final int quantity;
+  static int tmp=0;
+
 
   //@override
   //State<ScannedPart> createState() => ScannedPartState();
@@ -22,14 +25,50 @@ class ScannedPart extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: go,
-                child: const Text("Remove")
-              ), 
-              ElevatedButton(
-                onPressed: go,
-                child: const Text("Add"
-              ), 
+              SizedBox(
+               height: 40,
+               width: 100,
+               child:ElevatedButton(
+                onPressed: decrease,
+                child: const Text("Remove"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red
+                ),
+              ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+                ),
+                child:SizedBox(
+                width: 40,
+                height: 40,
+                child:Center(
+                  child:Card(
+                    color: Colors.blue,
+                    child:Text(tmp.toString()),
+                  )
+                )
+              ),
+              ),
+                SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                height: 40,
+                width: 100,
+                child:ElevatedButton(  
+                onPressed: increase,
+                child: const Text("Add"),
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green
+                ), ),
+                ),
+
               ],
           ),
         ],
@@ -37,7 +76,14 @@ class ScannedPart extends StatelessWidget {
     );
   }
 
-  void go() {
-    
+  void increase() {
+    tmp++;
+  }
+    void decrease() {
+    tmp--;
+  }
+  void getQuantity(){
+    tmp = quantity;
+
   }
 }

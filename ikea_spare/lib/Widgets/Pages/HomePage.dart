8588f82d4039981b42d66e75b6f8bf20e402b16f
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-10-14 11:13:49 +0200

    made classes of homepage widgets

--- ikea_spare/lib/Widgets/Pages/HomePage.dart
+++ ikea_spare/lib/Widgets/Pages/HomePage.dart
@@ -5,7 +5,6 @@ import 'package:ikea_spare/Widgets/CustomSearchBar.dart';
 import 'package:ikea_spare/Widgets/FilterButton.dart';
 import 'package:ikea_spare/Widgets/FilterButtonChoice.dart';
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -57,66 +56,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 onFilterChanged: _onFilterChanged,
               ),
             ),
-            Flexible(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                        maxWidth: width * 0.5,
-                        maxHeight: 40,
-                      ),
-                      child: const SparePartListHeader(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: ConstrainedBox(
-                              constraints: BoxConstraints(
-                                maxWidth: width * 0.5,
-                                maxHeight: height * 0.1,
-                              ),
-                              child: const Divider(
-                      thickness: 0.1,
-                      color: Colors.black,
-                      height: 5,
-                    ),
-                            ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 10,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                        maxWidth: width * 0.5,
-                        maxHeight: height,
-                      ),
-                      child: SparePartList(
-                        filter: selectedFilter,
-                        searchText: _searchText,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            HomePageSparePartListHeader(width: width * 0.5, height: 40),
+            HomePageSparePartListDivider(
+                width: width * 0.5, height: height * 0.1),
+            HomePageSparePartList(
+                width: width * 0.5,
+                height: height,
+                selectedFilter: selectedFilter,
+                searchText: _searchText),
           ],
         ),
       ),
@@ -124,3 +71,111 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class HomePageSparePartList extends StatelessWidget {
+  const HomePageSparePartList({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.selectedFilter,
+    required String searchText,
+  }) : _searchText = searchText;
+
+  final double width;
+  final double height;
+  final Filter selectedFilter;
+  final String _searchText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 10,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: width,
+                maxHeight: height,
+              ),
+              child: SparePartList(
+                filter: selectedFilter,
+                searchText: _searchText,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePageSparePartListDivider extends StatelessWidget {
+  const HomePageSparePartListDivider({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: width,
+                maxHeight: height,
+              ),
+              child: const Divider(
+                thickness: 0.1,
+                color: Colors.black,
+                height: 5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePageSparePartListHeader extends StatelessWidget {
+  const HomePageSparePartListHeader({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: width,
+                maxHeight: height,
+              ),
+              child: const SparePartListHeader(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

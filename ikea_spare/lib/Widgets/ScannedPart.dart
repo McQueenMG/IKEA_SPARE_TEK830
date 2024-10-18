ceb77f3c3db5f716ad9237ac075e8ed8cba48fe9
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-10-18 14:54:39 +0200

    Changed scanned part ui

--- ikea_spare/lib/Widgets/ScannedPart.dart
+++ ikea_spare/lib/Widgets/ScannedPart.dart
@@ -20,7 +20,6 @@ class _ScannedPartState extends State<ScannedPart> {
   int? input;
   Parts partsInstance = Parts();
 
-  // blah
   @override
   void initState() {
     super.initState();
@@ -29,112 +28,183 @@ class _ScannedPartState extends State<ScannedPart> {
   @override
   Widget build(BuildContext context) {
     SparePart part = partsInstance.getSparePartFromID(widget.id);
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
 
-    return SizedBox(
-      width: width,
-      height: height,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          color: Color.fromARGB(255, 131, 181, 223),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: width * 0.37,
-              height: height / 8,
-              child:
-                  ListCard(part: part, backgroundColor: Colors.lightBlue[50]!),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final double width = constraints.maxWidth;
+        final double height = constraints.maxHeight;
+
+        return SizedBox(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              color: const Color.fromARGB(255, 131, 181, 223),
+              borderRadius: BorderRadius.circular(10),
             ),
-            Padding(padding: EdgeInsets.only(top: 30)),
-            Container(
-              width: width * 0.37,
-              height: height / 2,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.lightBlue[50],
-              ),
-              child: Center(
-                child: Text(
-                  'Description: ${part.getDescription}',
-                  style: const TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(top: 15)),
-            Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 40,
-                  width: 100,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      removeQuantity(part);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                    ),
-                    child: const Text("Remove",
-                        style: TextStyle(fontSize: 12, color: Colors.black),
-                        textAlign: TextAlign.center),
+                Container(
+                  height: height * 0.3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[50]!,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.network(
+                        part.getImageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              part.getName,
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              part.getId,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            const Divider(
+                              color: Colors.black,
+                              thickness: 2,
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.inventory_2,
+                                  color: Colors.black,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Quantity: ${part.getQuantity.value}",
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.place_rounded,
+                                  color: Colors.black,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Location: ${part.getLocation}",
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(10),
+                Padding(padding: EdgeInsets.only(top: width * 0.05)),
+                SizedBox(
+                  width: width * 0.9,
+                  child: const Text(
+                    "Description:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
-                  child: SizedBox(
-                    width: 70,
-                    height: 60,
-                    child: Center(
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 5),
-                          labelText: 'Quantity',
-                          labelStyle:
-                              TextStyle(fontSize: 12, color: Colors.black),
-                        ),
-                        keyboardType: TextInputType.number,
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly
-                        ],
-                        onChanged: (value) {
-                          input = int.tryParse(value);
+                ),
+                Container(
+                  width: width * 0.9,
+                  height: height * 0.45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[50]!,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      part.getDescription,
+                      style: const TextStyle(fontSize: 18),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 15)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(
+                          width: width * 0.175, height: 40),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          removeQuantity(part);
                         },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        child: const Text("Remove",
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                            textAlign: TextAlign.center),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                SizedBox(
-                  height: 40,
-                  width: 100,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      addQuantity(part);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
+                    const SizedBox(width: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.tightFor(
+                            width: width * 0.1, height: 60),
+                        child: Center(
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 5),
+                              labelText: 'Quantity',
+                              labelStyle:
+                                  TextStyle(fontSize: 12, color: Colors.black),
+                            ),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            onChanged: (value) {
+                              input = int.tryParse(value);
+                            },
+                          ),
+                        ),
+                      ),
                     ),
-                    child: const Text("Add",
-                        style: TextStyle(fontSize: 12, color: Colors.black),
-                        textAlign: TextAlign.center),
-                  ),
+                    const SizedBox(width: 10),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tightFor(
+                          width: width * 0.175, height: 40),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          addQuantity(part);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                        ),
+                        child: const Text("Add",
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                            textAlign: TextAlign.center),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 
@@ -155,7 +225,6 @@ class _ScannedPartState extends State<ScannedPart> {
       }
       int tmp = part.getQuantity.value + input!;
       part.setQuantity(tmp);
-      
     });
   }
 }

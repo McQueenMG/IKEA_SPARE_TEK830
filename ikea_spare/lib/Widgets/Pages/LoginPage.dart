aad235a3d1d8ee5debdcf5cdb12f9d63c9bf2d8a
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-10-21 14:13:12 +0200

    Red outline when wrong input

--- ikea_spare/lib/Widgets/Pages/LoginPage.dart
+++ ikea_spare/lib/Widgets/Pages/LoginPage.dart
@@ -14,23 +14,37 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController()..text = "123";
   final String _userName = "ikea";
   final String _password = "123";
+  bool _wrongInput = false;
 
+  final userNameTextFieldDecoration = const InputDecoration(
+    focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue))
+  );
 
-  void openHomePage() {
-    if(_usernameController.text == _userName && _passwordController.text == _password) {
-      Navigator.of(context).push(MaterialPageRoute(
+  final passwordTextFieldDecoration = const InputDecoration(
+    focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue))
+  );
+
+  void _tryToOpenHomePage() {
+    if(_usernameController.text != _userName || _passwordController.text != _password) {
+      setState(() => _wrongInput = true);
+      return;
+    }
+
+    setState(() => _wrongInput = false);
+    _openHomePage();
+  }
+
+  void _openHomePage() {
+    Navigator.of(context).push(MaterialPageRoute(
           builder: (context) => const MyHomePage(
               title:'IKEA SPARE (Spare Parts acquisition and resource exchange)')
       ));
-      _passwordController.text = "";
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
@@ -45,7 +59,6 @@ class _LoginPageState extends State<LoginPage> {
                   child: Container(
                     height: 200,
                     width: 500,
-                    
                     decoration: BoxDecoration(
                       color: Colors.blue,
                       border: Border.all(color: Colors.blueAccent),
@@ -72,6 +85,10 @@ class _LoginPageState extends State<LoginPage> {
                                 child: TextField(
                                         controller: _usernameController,
                                         onChanged: (value) => {},
+                                        decoration: _wrongInput ? userNameTextFieldDecoration.copyWith(
+                                          enabledBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide(color: Colors.red))
+                                          ): userNameTextFieldDecoration,
                                       )
                               )
                             )
@@ -99,6 +116,10 @@ class _LoginPageState extends State<LoginPage> {
                                         obscureText: true,
                                         controller: _passwordController,
                                         onChanged: (value) => {},
+                                        decoration: _wrongInput ? userNameTextFieldDecoration.copyWith(
+                                          enabledBorder: const OutlineInputBorder(
+                                            borderSide: BorderSide(color: Colors.red))
+                                        ): userNameTextFieldDecoration,
                                       )
                               )
                             )
@@ -115,7 +136,7 @@ class _LoginPageState extends State<LoginPage> {
                         height: 40,
                         width: 200,
                         child: ElevatedButton(
-                          onPressed: openHomePage,
+                          onPressed: _tryToOpenHomePage,
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color.fromARGB(100, 0, 87, 173)),
                           child: const Text("Login", style: TextStyle(color: Colors.white)),
